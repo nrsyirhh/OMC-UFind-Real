3042879e36e43fabbbe56c7852a899c9e5d5e956
nrsyirhh <syiisyirah@gmail.com>
2025-07-01 22:56:09 +0800

    Add files via upload

--- FeedProject/Form1.vb
+++ FeedProject/Form1.vb
@@ -1,3 +1,25 @@
 ﻿Public Class Form1
+    Private Sub Panel2_Paint(sender As Object, e As PaintEventArgs)
 
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
 End Class
---
+++ FeedProject/ProfileForm.vb
@@ -0,0 +1,19 @@
+﻿Public Class ProfileForm
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Make the PicProfile circular
+        Dim circle As New Drawing2D.GraphicsPath()
+        circle.AddEllipse(0, 0, picProfile.Width, picProfile.Height)
+        picProfile.Region = New Region(circle)
+
+        ' Optional: Add a border effect
+        picProfile.BackColor = Color.White
+        picProfile.BorderStyle = BorderStyle.FixedSingle
+    End Sub
+
+    Private Sub picProfile_Click(sender As Object, e As EventArgs) Handles picProfile.Click
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
